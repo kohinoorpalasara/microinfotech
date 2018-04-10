54a363ace45228bd956ac73b5f625aa7754e5069
kohinoorpalasara <kohinoorpalasara@gmail.com>
2018-04-10 09:45:03 +0930

    init

---
+++ application/views/pages/home.php
@@ -0,0 +1,9 @@
+<div class="jumbotron" class="bg">
+  <h1 class="display-5" class="bg">Micro Infotech Pty Ltd</h1>
+  <p class="lead">Best Telecommunication Provider in the state</p>
+  <hr class="my-4">
+  <p>You and Us together make a new world with NBN Technology.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="<?php echo base_url(); ?>about" role="button">Learn more</a>
+  </p>
+</div>
